d02aeeef16ff480e54b992f72212e78c2f7e4208
Pekka Enberg <penberg@cs.helsinki.fi>
2009-11-14 21:31:56 +0200

    Fix large file compat code on Darwin
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

--- include/pstore/compat.h
+++ include/pstore/compat.h
@@ -9,14 +9,24 @@ char *strndup(const char *s, size_t n);
 
 #ifdef CONFIG_NEED_LARGE_FILE_COMPAT
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
 #include <stdio.h>
+
+#define O_LARGEFILE 0
 
 typedef off_t off64_t;
 
 static inline off64_t lseek64(int fd, off64_t offset, int whence)
 {
 	return lseek(fd, offset, whence);
+}
+
+#define stat64 stat
+
+static inline int fstat64(int fd, struct stat64 *st)
+{
+	return fstat(fd, st);
 }
 
 static inline FILE *fopen64(const char *filename, const char *type)
