35d14dff3d21b0d5ebebc8eaa20425bf7add5f78
Pekka Enberg <penberg@cs.helsinki.fi>
2010-01-30 10:29:53 +0200

    Fix include guard name and remove unused include in extent.h
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

--- include/pstore/extent.h
+++ include/pstore/extent.h
@@ -1,10 +1,8 @@
-#ifndef PSTORE_BLOCK_H
-#define PSTORE_BLOCK_H
+#ifndef PSTORE_EXTENT_H
+#define PSTORE_EXTENT_H
 
 #include "pstore/mmap-window.h"
 #include "pstore/column.h"
-
-#include <stdint.h>
 
 struct pstore_extent {
 	struct pstore_column	*parent;
@@ -17,4 +15,4 @@ void pstore_extent__delete(struct pstore_extent *self);
 struct pstore_extent *pstore_extent__read(struct pstore_column *column, int fd);
 void *pstore_extent__next_value(struct pstore_extent *self);
 
-#endif /* PSTORE_BLOCK_H */
+#endif /* PSTORE_EXTENT_H */
