021377a0142b4f2e3313e7c59e363b0ed73bd1b3
Pekka Enberg <penberg@cs.helsinki.fi>
2010-02-08 20:42:31 +0200

    Move pstore_extent__read() to extent.c
    Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

--- extent.c
+++ extent.c
@@ -1,4 +1,7 @@
 #include "pstore/extent.h"
+
+#include "pstore/disk-format.h"
+#include "pstore/read-write.h"
 #include "pstore/die.h"
 
 #include <sys/mman.h>
@@ -20,6 +23,25 @@ void pstore_extent__delete(struct pstore_extent *self)
 	mmap_window__unmap(self->mmap);
 
 	free(self);
+}
+
+#define MMAP_WINDOW_LEN		(128LL * 1024LL * 1024LL)	/* 128 MiB */
+
+struct pstore_extent *pstore_extent__read(struct pstore_column *column, int fd)
+{
+	struct pstore_file_extent f_extent;
+	struct pstore_extent *self;
+
+	self = pstore_extent__new();
+
+	seek_or_die(fd, column->f_offset, SEEK_SET);
+	read_or_die(fd, &f_extent, sizeof(f_extent));
+
+	self->mmap = mmap_window__map(MMAP_WINDOW_LEN, fd, column->f_offset + sizeof(f_extent), f_extent.size);
+
+	self->start = mmap_window__start(self->mmap);
+
+	return self;
 }
 
 void *pstore_extent__next_value(struct pstore_extent *self)
