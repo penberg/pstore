penberg/pstore

commit 7fc7055466cecbc1e3558a58fa8104bb397bd226

Don't use self->mmap in pstore_extent__decompress()
We have two kinds of extents: mmap'd and buffered extents. Don't use self->mmap
in pstore_extent__decompress() because we're dealing with buffered extents
there.

Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

compress.c
@@ -48,6 +48,7 @@ void pstore_extent__compress(struct pstore_extent *self, int fd)
 
 void pstore_extent__decompress(struct pstore_extent *self, int fd, off_t offset)
 {
+	struct mmap_window *mmap;
 	lzo_uint new_len;
 	void *out;
 	void *in;
@@ -57,7 +58,7 @@ void pstore_extent__decompress(struct pstore_extent *self, int fd, off_t offset)
 	 * We need extra PAGE_SIZE to maximum mmap window size because of mmap
 	 * alignment requirements.
 	 */
-	self->mmap	= mmap_window__map(self->psize + PAGE_SIZE, fd, offset + sizeof(struct pstore_file_extent), self->psize);
+	mmap		= mmap_window__map(self->psize + PAGE_SIZE, fd, offset + sizeof(struct pstore_file_extent), self->psize);
 	in		= mmap_window__start(self->mmap);
 
 	self->buffer	= buffer__new(self->lsize);
@@ -71,6 +72,5 @@ void pstore_extent__decompress(struct pstore_extent *self, int fd, off_t offset)
 	if (new_len != self->lsize)
 		die("decompression failed");
 
-	mmap_window__unmap(self->mmap);
-	self->mmap	 = NULL;
+	mmap_window__unmap(mmap);
 }

commit 12ba890d479331b2880ff6dc8320ae55f677be78

compress: Fix silly typo in pstore_extent__decompress()
This patch fixes breakage caused by commit
7fc7055466cecbc1e3558a58fa8104bb397bd226 ("Don't use self->mmap in
pstore_extent__decompress()").

Signed-off-by: Pekka Enberg <penberg@cs.helsinki.fi>

compress.c
@@ -59,7 +59,7 @@ void *pstore_extent__decompress(struct pstore_extent *self, int fd, off_t offset
 	 * alignment requirements.
 	 */
 	mmap		= mmap_window__map(self->psize + PAGE_SIZE, fd, offset + sizeof(struct pstore_file_extent), self->psize);
-	in		= mmap_window__start(self->mmap);
+	in		= mmap_window__start(mmap);
 
 	self->buffer	= buffer__new(self->lsize);
 	out		= buffer__start(self->buffer);