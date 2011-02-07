d1127a61aa618d62e1668cd702ad66dc87177dca
Karim Osman <karim.osman@reaktor.fi>
2011-02-07 18:40:36 +0200

    java: Add Column
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>
    Signed-off-by: Kare Nuorteva <kare.nuorteva@reaktor.fi>
    Signed-off-by: Riku Hurmalainen <riku.hurmalainen@reaktor.fi>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

---
+++ java/pstore/Column.java
@@ -0,0 +1,15 @@
+package pstore;
+
+public class Column {
+  final long ptr;
+
+  public Column(String name, long id, ValueType type) {
+    ptr = create(name, id, type.value);
+  }
+
+  Column(long ptr) {
+    this.ptr = ptr;
+  }
+
+  private native long create(String name, long id, int type);
+}
---
+++ java/pstore_Column.c
@@ -0,0 +1,18 @@
+#include "pstore_Column.h"
+
+#include "pstore/column.h"
+#include "pstore-jni.h"
+
+JNIEXPORT jlong JNICALL Java_pstore_Column_create(JNIEnv *env, jobject obj, jstring name, jlong id, jint type)
+{
+  const char *name0;
+  void *ptr;
+
+  name0 = (*env)->GetStringUTFChars(env, name, NULL);
+  if (!name0)
+    return PTR_TO_LONG(NULL);
+  ptr = pstore_column__new(name0, id, type);
+  (*env)->ReleaseStringUTFChars(env, name, name0);
+
+  return PTR_TO_LONG(ptr);
+}
