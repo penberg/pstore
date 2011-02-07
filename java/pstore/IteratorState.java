6947c5b83581bf670d411372d4c997d571c98338
Karim Osman <karim.osman@reaktor.fi>
2011-02-07 18:40:40 +0200

    java: Add IteratorState
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>
    Signed-off-by: Riku Hurmalainen <riku.hurmalainen@reaktor.fi>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

---
+++ java/pstore/IteratorState.java
@@ -0,0 +1,28 @@
+package pstore;
+
+import java.util.Iterator;
+
+public class IteratorState {
+  private Iterator<Row> rowIterator;
+  final long ptr;
+
+  public IteratorState(Iterator<Row> rowIterator) {
+    this.rowIterator = rowIterator;
+    ptr = create();
+  }
+
+  public void release() {
+    destroy(ptr);
+  }
+
+  private boolean hasNext() {
+    return rowIterator.hasNext();
+  }
+
+  private long next() {
+    return rowIterator.next().ptr;
+  }
+
+  private native long create();
+  private native void destroy(long ptr);
+}
