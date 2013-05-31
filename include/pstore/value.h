b6522649094dfaf2f6127635f085900ba6364bd2
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2013-05-31 14:41:18 +0300

    Add 'pstore_value_write'
    Couple the knowledge how to write a value to an extent to the value
    itself.
    
    Signed-off-by: Jussi Virtanen <jussi.k.virtanen@gmail.com>

--- include/pstore/value.h
+++ include/pstore/value.h
@@ -1,6 +1,8 @@
 #ifndef PSTORE_VALUE_H
 #define PSTORE_VALUE_H
 
+#include "pstore/buffer.h"
+
 #include <stddef.h>
 
 enum value_type {
@@ -11,5 +13,16 @@ struct pstore_value {
 	const char		*s;
 	size_t			len;
 };
+
+static inline void pstore_value_write(struct pstore_value *self, struct buffer *buf)
+{
+	buffer_append(buf, self->s, self->len);
+	buffer_append_char(buf, '\0');
+}
+
+static inline size_t pstore_value_write_length(struct pstore_value *self)
+{
+	return self->len + 1;
+}
 
 #endif /* PSTORE_VALUE_H */
